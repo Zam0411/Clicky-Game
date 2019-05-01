32e1f447301ab65aeb3e64ac032167f9e05332cb
Zam0411 <mzambrano41165@gmail.com>
2019-04-30 18:24:45 -0700

    Updates

---
+++ precache-manifest.11af90eda7c568f285afd90d388cc337.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a545a3f0cb055ebbca48c90e25f43c8",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "6479e4080d2d3d95e065",
+    "url": "/Clicky-Game/static/css/main.d1970105.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "6479e4080d2d3d95e065",
+    "url": "/Clicky-Game/static/js/main.e7ae766b.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
